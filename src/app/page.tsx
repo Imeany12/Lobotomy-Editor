88d5d6e0b4f0ce8508096a545b3176df865af47b
Imeany12 <oat.ueaangkun.inyaem@gmail.com>
2024-07-14 13:43:11 +0700

    feat: background gif

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,28 +8,30 @@ import SyntaxHighlighter from "react-syntax-highlighter";
 import { atomOneDarkReasonable } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import customStyle from "../constant/syntax-style";
 
-
-
-
 let rotten: { [key: string]: number } = {
-  "skibidi": 0,
-  "lookmaxing": 0,
-  "galvanize": 0,
-  "rizz": 0,
-  "sus": 0
+  skibidi: 0,
+  lookmaxing: 0,
+  galvanize: 0,
+  rizz: 0,
+  sus: 0,
 };
-
 
 export default function Page() {
   const [transformedText, setTransformedText] = useState<string>("");
+  const [sussyRNG, setSussyRNG] = useState<number>(
+    Math.floor(Math.random() * 10)
+  );
+  setInterval(() => {
+    setSussyRNG(Math.floor(Math.random() * 10));
+  }, 10000);
 
   function brainrot(event: React.ChangeEvent<HTMLTextAreaElement>) {
-    let rotten_string = transcribe(event.target.value)
+    let rotten_string = transcribe(event.target.value);
     for (const [key, value] of Object.entries(rotten)) {
-        rotten[key] = brainrot_counter(rotten_string, key);
+      rotten[key] = brainrot_counter(rotten_string, key);
     }
     setTransformedText(rotten_string);
-    console.log(Object.entries(rotten))
+    console.log(Object.entries(rotten));
   }
 
   return (
@@ -41,7 +43,15 @@ export default function Page() {
         <Textarea
           placeholder="Please type your command here"
           onChange={brainrot}
-          className="text-md"
+          className={`text-md  ${
+            rotten.galvanize >= 3
+              ? "bg-[url(https://media1.tenor.com/m/u_U_V-3kjyMAAAAd/little-john-galvanized-square-steel.gif)] bg-cover"
+              : ""
+          } ${
+            rotten.galvanize < 3 && sussyRNG > 5
+              ? "bg-[url(https://media1.tenor.com/m/WhXYjpREz1kAAAAd/skibidi-toilet-skibidi.gif)] bg-cover"
+              : ""
+          } `}
         />
         <SyntaxHighlighter
           style={atomOneDarkReasonable}
